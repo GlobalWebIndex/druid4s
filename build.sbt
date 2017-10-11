6cc4f048b1a720966de1c5b0c6c5ff38159888ab
l15k4 <liska.jakub@gmail.com>
2017-10-11 08:49:30 +0200

    upgrading to druid 0.10.1 + sbt changes

--- build.sbt
+++ build.sbt
@@ -3,14 +3,20 @@ version in ThisBuild := "0.0.4"
 crossScalaVersions in ThisBuild := Seq("2.12.3", "2.11.8")
 organization in ThisBuild := "net.globalwebindex"
 
-lazy val randagenVersion = "0.0.5"
+lazy val randagenVersion = "0.0.7"
 
 lazy val druid4s = (project in file("."))
+  .settings(aggregate in update := false)
+  .settings(publish := {})
   .aggregate(`druid4s-client`)
 
 lazy val `druid4s-client` = (project in file("client"))
   .enablePlugins(CommonPlugin)
-  .settings(name := "druid4s-client")
-  .settings(libraryDependencies ++= jodaTime ++ loggingApi ++ jackson ++ Seq(scalaHttp, loggingImplLog4j % "provided", scalatest))
-  .settings(publishSettings("GlobalWebIndex", "druid4s-client", s3Resolver))
-  .dependsOn(ProjectRef(uri(s"https://github.com/GlobalWebIndex/randagen.git#v$randagenVersion"), "randagen-core") % "compile->compile;test->test")
+  .settings(libraryDependencies ++=
+    Seq(
+      "net.globalwebindex" %% "randagen-core" % randagenVersion % "test",
+      scalaHttp,
+      loggingImplLog4j % "test",
+      scalatest
+    ) ++ jodaTime ++ loggingApi ++ jackson
+  ).settings(publishSettings("GlobalWebIndex", "druid4s-client", s3Resolver))
--- client/src/test/scala/gwi/druid/client/DruidIntegrationTestSuite.scala
+++ client/src/test/scala/gwi/druid/client/DruidIntegrationTestSuite.scala
@@ -6,18 +6,17 @@ import java.nio.file.{Files, Paths}
 import gwi.randagen._
 import org.joda.time.{DateTime, DateTimeZone}
 import org.scalatest.concurrent.ScalaFutures
-import org.scalatest.time.{Millis, Seconds, Span}
 import org.scalatest.{BeforeAndAfterAll, FreeSpec, Matchers}
 
 import scala.concurrent.Await
 import scala.concurrent.duration._
 
-class DruidClientTestSuite extends FreeSpec with ScalaFutures with Matchers with BeforeAndAfterAll with DruidBootstrap {
+class DruidIntegrationTestSuite extends FreeSpec with ScalaFutures with Matchers with BeforeAndAfterAll with DruidBootstrap {
   import SampleEventDefFactory._
   import Samples._
-  implicit val futurePatience = PatienceConfig(timeout =  Span(5, Seconds), interval = Span(200, Millis))
 
   lazy private val sampleSize = 10800
+  lazy private val targetDirPath = Paths.get("/tmp/druid4s-test")
 
   lazy private val from = new DateTime(2015, 1, 1, 0, 0, 0, DateTimeZone.UTC)
   lazy private val to = from.plusSeconds(sampleSize)
@@ -28,16 +27,25 @@ class DruidClientTestSuite extends FreeSpec with ScalaFutures with Matchers with
   lazy private val brokerClient = DruidClient.forQueryingBroker("localhost")(10.seconds, 1.minute)
   lazy private val overlordClient = DruidClient.forIndexing("localhost")(1.seconds, 5.seconds, 60.seconds)
 
-  lazy private val targetDirPath = Paths.get("/tmp/druid4s-test")
-  targetDirPath.toFile.mkdirs()
+  private def deleteDir(dir: File): Unit = {
+    if (dir.exists()) {
+      dir.listFiles.foreach { f =>
+        if (f.isDirectory)
+          deleteDir(f)
+        else
+          f.delete
+      }
+      Files.delete(dir.toPath)
+    }
+  }
 
   override def beforeAll() = {
+    deleteDir(targetDirPath.toFile)
+    targetDirPath.toFile.mkdirs()
     startDruidContainer()
-    Thread.sleep(20000) // Druid doesn't boot-up sooner unfortunately
-    Await.result(
-      RanDaGen.run(50 * 1024 * 1000, sampleSize, Parallelism(4), JsonEventGenerator, FsEventConsumer(targetDirPath, compress = true), SampleEventDefFactory()),
-      120.seconds
-    )
+    logger.info(s"Data generation initialized ...")
+    Await.ready(RanDaGen.run(50 * 1024 * 100, sampleSize, Parallelism(4), JsonEventGenerator, FsEventConsumer(targetDirPath, compress = true), SampleEventDefFactory()), 3.seconds)
+    Thread.sleep(25000)
     val hadoopTask = Samples.hadoopTask(segmentGrn, intervals, Granularity.HOUR, Granularity.HOUR, "yyyy/MM/dd/HH", targetDirPath.toAbsolutePath.toString)
     val result = overlordClient.postTask(hadoopTask).get
     logger.info(s"Indexing finished : ${result.status.status}")
@@ -47,21 +55,7 @@ class DruidClientTestSuite extends FreeSpec with ScalaFutures with Matchers with
     assertResult(TaskStatus.SUCCESS)(result.status.status)
   }
 
-  override def afterAll() = {
-    def deleteDir(dir: File): Unit = {
-      if (dir.exists()) {
-        dir.listFiles.foreach { f =>
-          if (f.isDirectory)
-            deleteDir(f)
-          else
-            f.delete
-        }
-        Files.delete(dir.toPath)
-      }
-    }
-    deleteDir(new File(s"$targetDirPath/2015"))
-    stopDruidContainer()
-  }
+  override def afterAll() = stopDruidContainer()
 
   "select" in {
     val response = brokerClient.postQuery(rawSelect(intervals), pretty = true).get.get
