00c4d26aafe576c9b58e578a266d4900e19dcf7a
lukasz.drygala <lukasz.drygala@gmail.com>
2019-09-03 11:20:42 +0200

    use staging druid

--- client/src/test/scala/gwi/druid/client/DruidIntegrationTestSuite.scala
+++ client/src/test/scala/gwi/druid/client/DruidIntegrationTestSuite.scala
@@ -10,17 +10,17 @@ import org.scalatest.{BeforeAndAfterAll, FreeSpec, Matchers}
 import scala.concurrent.Await
 import scala.concurrent.duration._
 
-class DruidIntegrationTestSuite extends FreeSpec with ScalaFutures with Matchers with BeforeAndAfterAll with LazyLogging {
+class DruidIntegrationTestSuite
+    extends FreeSpec
+    with ScalaFutures
+    with Matchers
+    with BeforeAndAfterAll
+    with LazyLogging {
   import SampleEventDefFactory._
   import Samples._
 
-  require(sys.env.get("GOOGLE_APPLICATION_CREDENTIALS").nonEmpty, "Test won't work without GOOGLE_APPLICATION_CREDENTIALS exported !!!")
-
-  val brokerHost = sys.env.getOrElse("BROKER_HOST", throw new IllegalStateException(s"BROKER_HOST env var must be defined !!!"))
-  val overlordHost = sys.env.getOrElse("OVERLORD_HOST", throw new IllegalStateException(s"BROKER_HOST env var must be defined !!!"))
-
-  val druidUser = sys.env("DRUID_USER")
-  val druidPassword = sys.env("DRUID_PASSWORD")
+  require(sys.env.get("GOOGLE_APPLICATION_CREDENTIALS").nonEmpty,
+          "Test won't work without GOOGLE_APPLICATION_CREDENTIALS exported !!!")
 
   lazy private val sampleSize = 10800
 
@@ -28,10 +28,17 @@ class DruidIntegrationTestSuite extends FreeSpec with ScalaFutures with Matchers
   lazy private val to = new DateTime(2015, 1, 1, 3, 0, 0, DateTimeZone.UTC)
 
   lazy private val segmentGrn = Granularity.HOUR
-  lazy private val intervals = segmentGrn.getIterable(from, to).map(_.toString).toList
+  lazy private val intervals =
+    segmentGrn.getIterable(from, to).map(_.toString).toList
 
-  lazy private val brokerClient = DruidClient.forQueryingBroker(s"$druidUser:$druidPassword@$brokerHost", port = 443, protocol = "https")(5.seconds, 1.minute)
-  lazy private val overlordClient = DruidClient.forIndexing(s"$druidUser:$druidPassword@$overlordHost", port = 443, protocol = "https")(5.seconds, 5.seconds, 3.minute)
+  lazy private val brokerClient =
+    DruidClient.forQueryingBroker("druid-broker-staging.in.globalwebindex.com",
+                                  port = 80,
+                                  protocol = "http")(5.seconds, 1.minute)
+  lazy private val overlordClient =
+    DruidClient.forIndexing("druid-overlord-staging.in.globalwebindex.com",
+                            port = 80,
+                            protocol = "http")(5.seconds, 5.seconds, 3.minute)
 
   require(brokerClient.isHealthy.get, "Broker is not healthy !!!")
   require(overlordClient.isHealthy.get, "Overlord is not healthy !!!")
@@ -40,7 +47,17 @@ class DruidIntegrationTestSuite extends FreeSpec with ScalaFutures with Matchers
     logger.info(s"Data generation initialized ...")
     val targetDirPath = "druid4s-test"
     val sourceDataBucket = "gwiq-view-s"
-    Await.ready(RanDaGen.run(50 * 1024 * 100, 10800, Parallelism(4), JsonEventGenerator, EventConsumer("gcs", s"$sourceDataBucket@$targetDirPath", compress = true), SampleEventDefFactory()), 3.seconds)
+    Await.ready(
+      RanDaGen.run(50 * 1024 * 100,
+                   10800,
+                   Parallelism(4),
+                   JsonEventGenerator,
+                   EventConsumer("gcs",
+                                 s"$sourceDataBucket@$targetDirPath",
+                                 compress = true),
+                   SampleEventDefFactory()),
+      3.seconds
+    )
     val hadoopTask =
       Samples.hadoopTask(
         segmentGrn,
@@ -53,7 +70,8 @@ class DruidIntegrationTestSuite extends FreeSpec with ScalaFutures with Matchers
     val result = overlordClient.postTask(hadoopTask).get
     logger.info(s"Indexing finished : ${result.status.status}")
     if (result.status.status != TaskStatus.SUCCESS)
-      logger.error(s"Indexing failed !!! ${result.errors.mkString("\n", "\n", "\n")}")
+      logger.error(
+        s"Indexing failed !!! ${result.errors.mkString("\n", "\n", "\n")}")
     assertResult(TaskStatus.SUCCESS)(result.status.status)
     Thread.sleep(8000) // data becomes queryable after a while, depending on segments poll duration
   }
@@ -61,7 +79,8 @@ class DruidIntegrationTestSuite extends FreeSpec with ScalaFutures with Matchers
   override def beforeAll(): Unit = indexTestData()
 
   "select" in {
-    val response = brokerClient.postQuery(rawSelect(intervals), pretty = true).get.get
+    val response =
+      brokerClient.postQuery(rawSelect(intervals), pretty = true).get.get
     val result = response.result
     val events = result.events
     val headEvent = events.head
@@ -74,46 +93,62 @@ class DruidIntegrationTestSuite extends FreeSpec with ScalaFutures with Matchers
   }
 
   "count" in {
-    val response = brokerClient.postQuery(countTimeSeries(intervals), pretty = true).get.get
+    val response =
+      brokerClient.postQuery(countTimeSeries(intervals), pretty = true).get.get
     assert(response.timestamp.nonEmpty)
     assertResult(10800)(response.result(countAggName))
   }
 
   "hll" in {
-    val response = brokerClient.postQuery(hllTimeSeries(intervals), pretty = true).get.get
+    val response =
+      brokerClient.postQuery(hllTimeSeries(intervals), pretty = true).get.get
     assert(response.timestamp.nonEmpty)
     val uniqueCount = response.result(uuidHllAggName)
     assert(5350 < uniqueCount && uniqueCount < 5450)
   }
 
   "sum" in {
-    val response = brokerClient.postQuery(sumTimeSeries(intervals), pretty = true).get.get
+    val response =
+      brokerClient.postQuery(sumTimeSeries(intervals), pretty = true).get.get
     assert(response.timestamp.nonEmpty)
     assertResult((0 until 10800).sum)(response.result(idxSumAggName))
   }
 
   "groupBy" in {
-    val responses = brokerClient.postQuery(groupBy(intervals), pretty = true).get
+    val responses =
+      brokerClient.postQuery(groupBy(intervals), pretty = true).get
     assertResult(4)(responses.size)
     val purchaseTypes = responses.map(_.event(purchaseFieldName).toString).toSet
-    val purchaseCounts = responses.map(_.event(countByPurchaseAggName).toString.toInt).toSet
+    val purchaseCounts =
+      responses.map(_.event(countByPurchaseAggName).toString.toInt).toSet
     assertResult(4)(purchaseTypes.size)
     assertResult(sampleSize)(purchaseCounts.sum)
   }
 
   "segmentMetadata" in {
-    val response = brokerClient.postQuery(segmentMetadata(intervals), pretty = true).get.get
+    val response =
+      brokerClient.postQuery(segmentMetadata(intervals), pretty = true).get.get
     val segmentColumns = response.columns.keySet
-    Set(sectionFieldName, purchaseFieldName, priceSumAggName, idxSumAggName, uuidHllAggName, countryFieldName, countAggName, "__time").foreach { field =>
+    Set(sectionFieldName,
+        purchaseFieldName,
+        priceSumAggName,
+        idxSumAggName,
+        uuidHllAggName,
+        countryFieldName,
+        countAggName,
+        "__time").foreach { field =>
       assert(segmentColumns.contains(field))
     }
   }
 
   "topN" in {
-    val response = brokerClient.postQuery(topN(intervals), pretty = true).get.get
+    val response =
+      brokerClient.postQuery(topN(intervals), pretty = true).get.get
     assert(response.timestamp.nonEmpty)
     assertResult(4)(response.result.size)
-    assertResult(weightedPurchasePMF.map(_._1).toSet)(response.result.map(_(purchaseFieldName)).toSet)
-    assertResult(sampleSize)(response.result.map(_(priceSumAggName).toString.toInt).sum)
+    assertResult(weightedPurchasePMF.map(_._1).toSet)(
+      response.result.map(_(purchaseFieldName)).toSet)
+    assertResult(sampleSize)(
+      response.result.map(_(priceSumAggName).toString.toInt).sum)
   }
 }
