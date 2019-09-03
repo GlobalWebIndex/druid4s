7d0a8174b253d3fbe23f9d2b89b3a024339bd36f
lukasz.drygala <lukasz.drygala@gmail.com>
2019-09-03 13:46:10 +0200

    try to fix bintray deploy

--- project/Deploy.scala
+++ project/Deploy.scala
@@ -7,14 +7,23 @@ object Deploy {
     publishArtifact := true,
     publishMavenStyle := true,
     publishArtifact in Test := false,
-    organization := "net.globalwebindex",
-    homepage := Some(url(s"https://github.com/$ghOrganizationName/$ghProjectName/blob/master/README.md")),
+    organization := "gwidx",
+    homepage := Some(url(
+      s"https://github.com/$ghOrganizationName/$ghProjectName/blob/master/README.md")),
     licenses in ThisBuild += ("MIT", url("http://opensource.org/licenses/MIT")),
-    developers += Developer("l15k4", "Jakub Liska", "liska.jakub@gmail.com", url("https://github.com/l15k4")),
-    scmInfo := Some(ScmInfo(url(s"https://github.com/$ghOrganizationName/$ghProjectName"), s"git@github.com:$ghOrganizationName/$ghProjectName.git")),
-    bintrayVcsUrl := Some(s"git@github.com:$ghOrganizationName/$ghProjectName.git"),
-    bintrayRepository := ghOrganizationName,
-    pomIncludeRepository := { _ => false },
+    developers += Developer("l15k4",
+                            "Jakub Liska",
+                            "liska.jakub@gmail.com",
+                            url("https://github.com/l15k4")),
+    scmInfo := Some(
+      ScmInfo(url(s"https://github.com/$ghOrganizationName/$ghProjectName"),
+              s"git@github.com:$ghOrganizationName/$ghProjectName.git")),
+    bintrayVcsUrl := Some(
+      s"git@github.com:$ghOrganizationName/$ghProjectName.git"),
+    bintrayRepository := "maven",
+    pomIncludeRepository := { _ =>
+      false
+    },
     pomExtra :=
       <url>https://github.com/{ghOrganizationName}/{ghProjectName}</url>
         <licenses>
