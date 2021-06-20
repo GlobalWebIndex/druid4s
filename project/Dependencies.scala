5ab83b0d86d183b9677cf8b154563edad0c7141a
DMP Scala Steward <dmp@globalwebindex.com>
2021-06-20 00:32:59 +0000

    Update scala-logging to 3.9.4

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -12,7 +12,7 @@ object Dependencies {
 
   lazy val loggingApi                   = Seq(
     "org.slf4j"                     %     "slf4j-api"                             % "1.7.30",
-    "com.typesafe.scala-logging"    %%    "scala-logging"                         % "3.9.2"
+    "com.typesafe.scala-logging"    %%    "scala-logging"                         % "3.9.4"
   )
 
   lazy val jodaTime                     = Seq(
